1ee43d4377ab3ac753bf42da57d855fe97cfb54a
John DeMay <36252901+demayj10@users.noreply.github.com>
2022-02-26 21:15:34 -0800

    Games are now sortable by game title from A-Z (#27)

--- src/components/GamesCollection/GamesCollection.tsx
+++ src/components/GamesCollection/GamesCollection.tsx
@@ -1,17 +1,74 @@
-import React, { FC } from 'react';
+import React, {
+  FC, useState,
+} from 'react';
 import './GamesCollection.css';
-
+import {
+  Box,
+  FormControl,
+  Grid, InputLabel, MenuItem, Select, SelectChangeEvent,
+} from '@mui/material';
 import { useAppSelector } from '../../app/hooks';
 import { selectAllGames } from '../../AppSlice';
 import { GameCard } from '../GameCard/GameCard';
+import { GameData } from '../../types';
+
+const enum SortType {
+  GameTitle = 'Game Title',
+}
+
+const compareGameNames = (a: GameData, b: GameData) => {
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
+  return 0;
+};
+
+const sortGamesList = (sortBy: string, games: GameData[]): GameData[] => {
+  switch (sortBy) {
+    case SortType.GameTitle:
+      games.sort(compareGameNames);
+      break;
+    default:
+      break;
+  }
+
+  return games;
+};
 
 export const GamesCollection: FC = () => {
   const games = useAppSelector(selectAllGames);
+  const [sortBy, setSortBy] = useState<string>('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSortBy(event.target.value);
+  };
+
+  const sortedGames: GameData[] = sortGamesList(sortBy, [...games]);
 
   return (
     <div id="games-collection-container">
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-end"
+        alignItems="center"
+        marginTop="1em"
+      >
+        <Box sx={{ width: 100, mr: 3 }}>
+          <FormControl fullWidth variant="standard">
+            <InputLabel>Sort By</InputLabel>
+            <Select
+              value={sortBy}
+              onChange={handleChange}
+              label="Sort By"
+              defaultValue=""
+            >
+              <MenuItem value={SortType.GameTitle}>{SortType.GameTitle}</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      </Grid>
       <div id="card-container">
-        {games.map((game) => (
+        {sortedGames.map((game) => (
           <GameCard
             key={game.appid}
             id={game.appid}
